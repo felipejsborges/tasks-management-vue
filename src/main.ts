7300e0e8284d257c1c63d2a171461e575a8a3c5d
Felipe Borges <felipejsborges@outlook.com>
2023-05-12 02:32:54 -0300

    chore: setup vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
 app.use(router)
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
+app.use(vuetify)
 
 app.mount('#app')
